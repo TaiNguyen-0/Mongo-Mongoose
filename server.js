d534f4cc5fe7112ea2029521f2c048f36f154b71
Tai <taing89@gmail.com>
2019-08-10 18:30:49 -0700

    Start Scrape

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ var expressHandlebars = require("express-handlebars");
 var bodyParser = require("body-parser");
 // var logger = require("morgan");
 var mongoose = require("mongoose");
+var PORT = 3000 || process.env.PORT;
+
+// Initialize Express
+var app = express();
+// Sets up a Express Route
+var router = express.Router();
+
+require("./config/routes")(router);
+// Designate the publick folder as a static folder
+app.use(express.static(__dirname + "/public"));
 
 // Connects Handlebars w/ Express app
 app.engine("handlebars", expressHandlebars({
@@ -13,28 +23,24 @@ app.set("view engine", "handlebars");
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-var axios = require("axios");
-var cheerio = require("cheerio");
+
+// Have every request go through our router middleware
+app.use(router);
+
+// Deployed
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
+// // Our scraping tools
+// // Axios is a promised-based http library, similar to jQuery's Ajax method
+// // It works on the client and on the server
+// var axios = require("axios");
+// var cheerio = require("cheerio");
 
 // Require all models
 // var db = require("./models");
 
-var PORT = 3000 || process.env.PORT;
 
-// Initialize Express
-var app = express();
 
-// Sets up a Express Route
-var router = express.Router();
-
-// Designate the publick folder as a static folder
-app.use(express.static(__dirname + "/public"));
-
-// Have every request go through our router middleware
-app.use(router);
 
 // Start the server
 app.listen(PORT, function() {
