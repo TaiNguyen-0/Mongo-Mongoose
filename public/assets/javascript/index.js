765e21436ca0388a1a9093a24aa3a9281d53683e
Tai <taing89@gmail.com>
2019-08-11 21:11:27 -0700

    saved.js adding funcs

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -59,4 +59,56 @@ $(document).ready(function() {
             ].join(""));
         articleContainer.append(emptyAlert);
     }
+
+    // function handleArticleSave() {
+    //     var articlePanels = [];
+
+    //     for (var i = 0; i < articles.length; i++) {
+    //         articlePanels.push(createPanel(articles[i]));
+    //     }
+    //     articleContainer.append(articlePanels);
+    // }
+
+    function createPanel(article) {
+        var panel =
+            $(["<div class='panel panel-default'>",
+                "<div class='panel-heading'><h3>",
+                article.headline,
+                "<a class='btn btn-success save'>",
+                "Save Article</a></h3></div>",
+                "<div class='panel-body'>",
+                article.summary,
+                "</div></div>"
+            ].join(""));
+        panel.data("_id", article._id);
+
+        return panel;
+    }
+
+    function renderEmpty() {
+
+    }
+
+    function handleArticleSave() {
+        var articleToSave = $(this).parents(".panel").data();
+        articleToSave.saved = true;
+        $.ajax({
+                method: "PATCH",
+                url: "/api/headlines",
+                data: articleToSave
+            })
+            .then(function(data) {
+                if (data.ok) {
+                    initPage();
+                }
+            });
+    }
+
+    function handleArticleScrape() {
+        $.get("/api/fetch")
+            .then(function(data) {
+                initPage();
+                bootbox.alert("<h3 class='text-center m-top-70'>" + data.message + "</h3>")
+            })
+    }
 })
