00fcc29946bb72660f8eab2bee03c36d821b0434
Tai <taing89@gmail.com>
2019-08-11 21:45:43 -0700

    break

--- public/assets/javascript/saved.js
+++ public/assets/javascript/saved.js
@@ -56,7 +56,54 @@ $(document).ready(function() {
     }
 
     function renderNotesList(data) {
-
+        var notesToRender = [];
+        var currentNote;
+        if (!data.notes.length) {
+            currentNote = [
+                "<li class='list-group-item'>",
+                "No notes for this article yet.</li>",
+            ].join("");
+            notesToRender.push(currentNote);
+        } else {
+            for (var i = 0; i < data.notes.length; i++) {
+                currentNote = $([
+                    "<li class='list-group-item note'>",
+                    data.notes[i].noteText,
+                    "<button class='btn btn-danger note-delete'>x</button></li>",
+                ].join(""));
+                currentNote.children("button").data("_id", data.notes[i]._id);
+                notesToRender.push(currentNote);
+            }
+        }
+        $(".note-container").append(notesToRender);
     }
 
+    function handleArticleDelete() {
+        var articleToDelete = $(this).parents(".panel").data();
+        $.ajax({
+            method: "DELETE",
+            url: "/api/headlines/" + articleToDelete._id
+        }).then(function(data) {
+            if (data.ok) {
+                initPage();
+            }
+        });
+    }
+
+    function handleArticleNotes() {
+        var currentArticle = $(this).parents(".panel").data();
+        $.get("/api/notes" + currentArticle._id).then(function(data) {
+            var modalText = [
+                "<div class='container-fluid text-center'>",
+                "<h4>Notes for article: ",
+                currentArticle._id,
+                "</h4>",
+                "<hr />",
+                "<ul class='list-group note-container'>",
+                "</ul>",
+                "<textarea placeholder='New Note' rows='4' cols='60'></textarea>",
+                "<button class='btn btn-success save'>Save Note</button>",
+            ]
+        })
+    }
 })
